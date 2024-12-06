5447163ad95f7f80310eca1f3ec58d8a7dabf5b1
eliesjebri <elies.jebri@gmail.com>
2024-12-06 09:51:43 +0100

    updates

--- Labs/2-Docker/main.go
+++ Labs/2-Docker/main.go
@@ -1,7 +1,25 @@
 package main
+  
+import (
+  "fmt"
+  "net/http"
+)
 
-import "fmt"
+const (
+  port = ":80"
+)
 
-func main() {
-	fmt.Println("Hello world")
+var calls = 0
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+  calls++
+  fmt.Fprintf(w, "Hello, world! You have called me %d times.\n", calls)
 }
+
+func init() {
+  fmt.Printf("Started server at http://localhost%v.\n", port)
+  http.HandleFunc("/", HelloWorld)
+  http.ListenAndServe(port, nil)
+}
+
+func main() {}
